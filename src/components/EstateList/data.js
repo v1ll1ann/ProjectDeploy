a417beaa123d154c31007caead0359acfd8e69f2
v1ll1ann <tanawat.sritr@bumail.net>
2024-11-26 18:20:36 +0700

    Update File

--- src/components/EstateList/data.js
+++ src/components/EstateList/data.js
@@ -1,22 +1,22 @@
 export const featured = [
     {
-        cover: "../../public/assets/SingleHouse.png",
+        cover: "/assets/SingleHouse.png",
         name: "บ้านเดี่ยว",
     },
     {
-        cover: "../../public/assets/TownHouse.png",
+        cover: "/assets/Townhouse.png",
         name: "ทาวน์เฮ้าส์",
     },
     {
-        cover: "../../public/assets/Apartment.png",
+        cover: "/assets/Apartment.png",
         name: "ห้องชุดพักอาศัย",
     },
     {
-        cover: "../../public/assets/CommercialBuilding.png",
+        cover: "/assets/CommercialBuilding.png",
         name: "อาคารพาณิชย์",
     },
     {
-        cover: "../../public/assets/Land.png",
+        cover: "/assets/Land.png",
         name: "ที่ดิน",
     },
-]
+];
